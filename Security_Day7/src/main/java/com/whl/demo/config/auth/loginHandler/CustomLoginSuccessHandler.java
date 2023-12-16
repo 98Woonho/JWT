955e9dbf9a05a60576c33290309cfe11e7aaf37f
98Woonho <9woonho8@gmail.com>
2023-12-16 13:55:57 +0900

    2023-12-15

---
+++ Security_Day7/src/main/java/com/whl/demo/config/auth/loginHandler/CustomLoginSuccessHandler.java
@@ -0,0 +1,39 @@
+package com.whl.demo.config.auth.loginHandler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    // /login post로 submit 했을 때
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        Collection<? extends GrantedAuthority> collection = authentication.getAuthorities();
+        System.out.println("[CustomLoginSuccessHandler] onAuthenticationSuccess()!!!");
+        collection.forEach(
+                (role) -> {
+                    // System.out.println("[CustomLoginSuccessHandler] onAuthenticationSuccess role : " + role); 로그인 시 넘어오는 role 테스트
+                    String role_str = role.getAuthority();
+                    try {
+                        if(role_str.equals("ROLE_USER")) {
+                            response.sendRedirect("/user"); // role = ROLE_USER이면 user 페이지로 Redirect
+                        } else if(role_str.equals("ROLE_MEMBER")) {
+                            response.sendRedirect("/member"); // role = ROLE_MEMBER이면 user 페이지로 Redirect
+                        } else if(role_str.equals("ROLE_ADMIN")) {
+                            response.sendRedirect("/admin"); // role = ROLE_ADMIN이면 user 페이지로 Redirect
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+    }
+
+}
---
+++ Security_Day7/src/main/java/com/whl/demo/config/auth/logoutHandler/CustomLogoutSuccessHandler.java
@@ -0,0 +1,58 @@
+package com.whl.demo.config.auth.logoutHandler;
+
+import com.whl.demo.config.auth.PrincipalDetails;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.io.IOException;
+
+
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+    // 카카오 계정과 함께 로그아웃
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}") // [공부] @Value : application.properties의 property 값을 필드에 주입해주는 Annotation. @Value를 사용할려면 @Value를 사용하고 있는 메서드의 클래스가 Bean으로 등록이 되어 있어야 함.
+    private String kakaoClientId; // 로그아웃에 필요한 client_id(앱 REST KEY)
+
+    private final String Redirect_Uri = "http://localhost:8080/login"; // 로그아웃에 필요한 logout_redirect_uri([내 애플리케이션] > [카카오 로그인] > [Logout Redirect URI]에 등록된 값 중 하나)
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        System.out.println("[CustomLogoutSuccessHandler] onLogoutSuccess()");
+
+        // provider 가져오기
+        PrincipalDetails principalDetails = (PrincipalDetails)authentication.getPrincipal();
+        String provider = principalDetails.getUserDto().getProvider();
+        System.out.println("[CustomLogoutSuccessHandler] logout() provider : " + provider);
+
+        // provider에 따른 로그아웃 설정
+        if (provider != null && provider.equals("kakao")) {
+            // https://kauth.kakao.com/oauth/logout 링크에 client_id와 logout_redirect_uri를 파라미터로 보내주면 GET으로 처리해서 카카오 계정과 함께 로그아웃을 진행해줌.
+            String url = "https://kauth.kakao.com/oauth/logout?client_id=" + kakaoClientId + "&logout_redirect_uri=" + Redirect_Uri;
+            response.sendRedirect(url);
+
+            return ;
+        } else if (provider != null && provider.equals("naver")) {
+            // "https://nid.naver.com/nidlogin.logout?returl=http://localhost:8080/" 을 해도 네이버로 이동하는 이유는, 네이버 측에서 returl에 네이버 관련 링크가 아니면 naver.com로 redirect되도록 설정을 해놓았기 때문이다.=
+            String url = "https://nid.naver.com/nidlogin.logout";
+            response.sendRedirect(url);
+            return ;
+        } else if (provider != null && provider.equals("google")) {
+            String url = "http://accounts.google.com/Logout";
+            response.sendRedirect(url);
+            return ;
+        }
+
+        // 일반 로그아웃
+        HttpSession session = request.getSession(false); // session을 가져오는데, session이 없으면 새로 생성하지 말고 null을 반환
+        if(session != null) {
+            session.invalidate(); // 로그아웃 시, session 데이터 제거
+        }
+
+        response.sendRedirect("/"); // request.getContextPath() == "/";
+    }
+}
